3743fd7453ead8c3a0cdf74389dca6c1c1a11c8c
JulienBoileau <106658295+JulienBoileau@users.noreply.github.com>
2022-12-23 23:08:20 +0100

    Add files via upload

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import Logo from "../../assets/LOGO.png";
 import "./_Footer.css";
 
 export default function Footer() {
@@ -9,7 +8,7 @@ export default function Footer() {
     <footer className="footer">
     
       <Link className="logo_container" to="/">
-        <Logo fill="#ffffff" className="logo" />
+        <img src="../../assets/LOGO.png" className="logo_footer" alt="Logo Kasa" />
       </Link>
 
       <div className="mentions">© 2020 Kasa. All rights reserved</div>
