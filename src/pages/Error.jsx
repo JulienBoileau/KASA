23695ae8fd8e2803c2cc2bfbdafc48d1e2b5844e
JulienBoileau <julien.boileau.01@gmail.com>
2022-12-23 09:07:32 +0100

    MAJ et ajout fichiers components

---
+++ src/pages/Error.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./_Error.css";
+
+export default function Error() {
+  
+    return (
+    
+    <div className="error_container">
+      <div className="error_number">404</div>
+      <div className="error_text">Oups! La page que vous demandez n'existe pas.</div>
+      <Link className="error_link" to="/" title="Retour vers page d'accueil">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
+  
+  );
+}
