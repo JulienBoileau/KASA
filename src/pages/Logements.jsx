7565abbfdba391e72b9b69378b0a0c61c0f6b346
JulienBoileau <106658295+JulienBoileau@users.noreply.github.com>
2022-12-27 13:45:23 +0100

    Add files via upload

--- src/pages/Logements.jsx
+++ src/pages/Logements.jsx
@@ -6,12 +6,13 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import Carrousel from '../components/Carrousel/Carrousel';
-import Collapse from '../components/Collapse/Collapse';
-//import Host from '../components/Host_and_rate/Host';
-//import Rate from '../components/Host_and_rate/Rate';
-import Error from './Error';
+import Collapse from '../components/Collapse/Collapse_logement';
+import Host from '../components/Host_and_rate/Host';
+import Rate from '../components/Host_and_rate/Rate';
+//import Error from './Error';
 import Footer from '../components/Footer/Footer';
 import LogementTags from '../components/Logement_tags/Logement_tags';
+import { useNavigate } from "react-router-dom";
 
 // Fonction principale pour l'affichage du logement sélectionné
 
@@ -19,20 +20,22 @@ export default function Logement() {
 
     const [logement, setLogement] = useState();
     const params = useParams()
+    const navigate = useNavigate();
 
     useEffect(() => {
         fetch('http://localhost:3000/data/logements.json')
           .then(res => res.json())
           .then(data => {
             const logement = data.find((item) => item.id === params.id);
-            console.log(logement.rating)
-            console.log(logement.host)
+            //console.log(logement.rating)
+            //console.log(logement.host)
+            //console.log(logement.tags)
             if (!logement) {
-                return <Error />;
+                navigate('/not-found')
             }
             setLogement(logement)
           });
-      }, ); 
+      }, []); 
 
     return (
         
@@ -45,18 +48,18 @@ export default function Logement() {
                                 <h1 className="logement_title">{logement &&  logement.title}</h1>
                                 <p className="logement_location">{logement &&  logement.location}</p>
                                 <div className="logement_tags">
-                                {logement && logement.tags.map((tag, index) => (<LogementTags key={index} getTag={tag} /> ))}
+                                {logement && logement.tags.map((tag, index) => (<LogementTags key={index} tag={tag} /> ))}
                                 </div>
                             </div>
                             <div className="logement_rate_host">
-                            {/*logement && <Rate rating={logement.rating} />*/}
-                            {/*logement && <Host host={logement.host} /> */}
+                                {logement && <Host host={logement.host} />}
+                                {logement && <Rate rating={logement.rating} />}
                             </div>
                         </div>
                         <div className="logement_collapses">
-                            <Collapse title="description" content={logement && logement.description} />
-                            <Collapse title="equipements" content={logement && (logement.equipments).map((equipment, index) => (
-                                <li className="equipements_liste" key={index}>{equipment}</li>))}/>
+                            <Collapse title="description" name="Description" description={logement && logement.description} />
+                            <Collapse title="equipements" name="Equipements" list={logement && (logement.equipments).map((equipment, index) => (
+                              <li className="equipements_liste" key={index}>{equipment}</li>))}/>
                         </div>
                 </section>
             <Footer />
