cbec523847580e7a8ba9781424beb4e83c5b7822
JulienBoileau <106658295+JulienBoileau@users.noreply.github.com>
2022-12-28 15:46:53 +0100

    Add files via upload

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Header from "../components/Header/Header";
 import "./_Error.css";
 
 export default function Error() {
   
     return (
-    
-    <div className="error_container">
-      <div className="error_number">404</div>
-      <div className="error_text">Oups! La page que vous demandez n'existe pas.</div>
-      <Link className="error_link" to="/" title="Retour vers page d'accueil">
-        Retourner sur la page d'accueil
-      </Link>
+      <div>
+        <Header />
+        <div className="error_container">
+          <div className="error_number">404</div>
+          <div className="error_text">Oups! La page que vous demandez n'existe pas.</div>
+          <Link className="error_link" to="/" title="Retour vers page d'accueil">
+            Retourner sur la page d'accueil
+          </Link>
+        </div>
     </div>
   
   );
