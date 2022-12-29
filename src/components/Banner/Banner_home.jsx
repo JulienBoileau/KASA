f69e178b9f2766aaa4e8763a8ebc896dbd9e4497
JulienBoileau <106658295+JulienBoileau@users.noreply.github.com>
2022-12-29 09:54:52 +0100

    Add files via upload

--- src/components/Banner/Banner_home.jsx
+++ src/components/Banner/Banner_home.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import './_Banner.css';
 
-export default function BannerHome(props) {
+export default function BannerHome() {
     return (
         <div>
         <section className="banner">
-            {/*<p>{props.origin}</p>*/}
             <div className="banner-container">
                 <div className="banner_home_img">
                     <img src="/assets/BANNER_HOME.png" alt='banniere_kasa'/>
